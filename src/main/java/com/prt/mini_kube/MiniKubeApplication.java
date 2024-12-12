87ac65bdb23b8cab2640ba2fc0212b565dd27def
prathmeshthorat <prathmeshthorat06@gmail.com>
2024-12-12 23:54:07 +0530

    first commit for mini-kube

---
+++ src/main/java/com/prt/mini_kube/MiniKubeApplication.java
@@ -0,0 +1,13 @@
+package com.prt.mini_kube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniKubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniKubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prt/mini_kube/MiniKubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prt.mini_kube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniKubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
